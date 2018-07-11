5785007b630e721b719f8df5064e2258ceab9013
Romain Lesur <romain.lesur@justice.gouv.fr>
2018-07-11 18:12:26 +0200

    remove weasyprint tests

--- tests/testthat/test-wpdf_document_base.R
+++ tests/testthat/test-wpdf_document_base.R
@@ -1,5 +1,9 @@
 context("test-wpdf_document_base.R")
 
+test_that("Pandoc version is OK", {
+  expect_true(rmarkdown::pandoc_available("2.1.3"))
+})
+
 test_that("Render with Prince", {
   rmarkdown::render("document.Rmd", wpdf_document_base(wpdf_engine = "prince"), "prince.pdf")
   knitr::knit_meta()
@@ -8,9 +12,9 @@ test_that("Render with Prince", {
   expect_match(pdftools::pdf_text("prince.pdf"), "Created with Javascript")
 })
 
-test_that("Render with WeasyPrint", {
-  rmarkdown::render("document.Rmd", wpdf_document_base(wpdf_engine = "weasyprint"), "weasyprint.pdf")
-  knitr::knit_meta()
-  expect_true(file.exists("weasyprint.pdf"))
-})
+# test_that("Render with WeasyPrint", {
+#   rmarkdown::render("document.Rmd", wpdf_document_base(wpdf_engine = "weasyprint"), "weasyprint.pdf")
+#   knitr::knit_meta()
+#   expect_true(file.exists("weasyprint.pdf"))
+# })
 
