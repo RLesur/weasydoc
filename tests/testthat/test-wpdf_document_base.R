a6751eeb64d95224fe863fe78cdb89339e9eaec1
Romain Lesur <romain.lesur@gmail.com>
2018-07-21 23:37:42 +0200

    test keep_html option

--- tests/testthat/test-wpdf_document_base.R
+++ tests/testthat/test-wpdf_document_base.R
@@ -4,6 +4,7 @@ test_that("Render with WeasyPrint-Default options", {
   rmarkdown::render("document.Rmd", wpdf_document_base(), "weasyprint.pdf")
   knitr::knit_meta()
   expect_true(file.exists("weasyprint.pdf"))
+  unlink("weasyprint.pdf")
 })
 
 test_that("Render with Prince-Default options", {
@@ -12,6 +13,7 @@ test_that("Render with Prince-Default options", {
   expect_true(file.exists("prince.pdf"))
 
   expect_match(pdftools::pdf_text("prince.pdf"), "Created with Javascript")
+  unlink("prince.pdf")
 })
 
 test_that("Attach Rmd file correctly", {
@@ -22,4 +24,20 @@ test_that("Attach Rmd file correctly", {
 
   testthat::expect_equal(pdftools::pdf_attachments("weasyprint.pdf")[[1]]$name, "document.Rmd")
   testthat::expect_equal(pdftools::pdf_attachments("prince.pdf")[[1]]$name, "document.Rmd")
+  unlink("weasyprint.pdf")
+  unlink("prince.pdf")
+})
+
+test_that("Keep html option", {
+  rmarkdown::render("document.Rmd", wpdf_document_base(keep_html = TRUE), "weasyprint.pdf")
+  knitr::knit_meta()
+  expect_true(file.exists("weasyprint.html"))
+  unlink("weasyprint.pdf")
+  unlink("weasyprint.html")
+
+  rmarkdown::render("document.Rmd", wpdf_document_base(wpdf_engine = "prince", keep_html = TRUE), "prince.pdf")
+  knitr::knit_meta()
+  expect_true(file.exists("prince.html"))
+  unlink("prince.pdf")
+  unlink("prince.html")
 })
