56059bdb469dd258104092a2fcdeb54ea40437cb
Romain Lesur <romain.lesur@gmail.com>
2018-08-25 23:56:41 +0200

    resolved using bookdown tricks

--- R/html2pdf.R
+++ R/html2pdf.R
@@ -1,21 +1,32 @@
 #' Add conversion to pdf
 #'
+#' @param ... Arguments to be passed to a specific output format function.
 #' @param base_format Any `HTML` format.
 #'
 #' @return R Markdown output format to pass to `rmarkdown::render`.
 #' @export
-html2pdf <- function(base_format) {
-  post_processor <- function(metadata, input_file, output_file, clean, verbose) {
+html2pdf <- function(..., base_format = rmarkdown::html_document) {
+  base_format <- get_base_format(base_format)
+  config = base_format(...)
+
+  post <- config$post_processor
+  config$post_processor <- function(metadata, input_file, output_file, clean, verbose) {
+    if (is.function(post)) {
+      output_file <- post(metadata, input_file, output_file, clean, verbose)
+    }
     output <- paste0(tools::file_path_sans_ext(output_file), ".pdf")
     system2("prince", c("--javascript", output_file, "-o", output))
     output
   }
 
-  rmarkdown::output_format(
-    knitr = NULL,
-    pandoc = NULL,
-    clean_supporting = FALSE,
-    post_processor = post_processor,
-    base_format = base_format
-  )
+  config
+}
+
+get_base_format <- function(format) {
+  if (is.character(format)) {
+    format = eval(parse(text = format))
+  }
+  if (!is.function(format))
+    stop("The output format must be a function")
+  format
 }
