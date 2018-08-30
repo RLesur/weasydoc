c4f8f699509692f7d5cea44f585113ea66293b68
Romain Lesur <romain.lesur@justice.gouv.fr>
2018-08-30 15:24:08 +0200

    update tests

--- tests/testthat/test-make_pdf.R
+++ tests/testthat/test-make_pdf.R
@@ -1,13 +1,13 @@
 context("test-make_pdf.R")
 
 test_that("Direct PDF generation with WeasyPrint", {
-  pdf_file <- make_pdf("test folder/html_file.html")
+  pdf_file <- make_pdf("test_folder/html_file.html")
   expect_true(file.exists(pdf_file))
   unlink(pdf_file)
 })
 
 test_that("Direct PDF generation with Prince", {
-  pdf_file <- make_pdf("test folder/html_file.html", engine = "prince")
+  pdf_file <- make_pdf("test_folder/html_file.html", engine = "prince")
   expect_true(file.exists(pdf_file))
   unlink(pdf_file)
 })
